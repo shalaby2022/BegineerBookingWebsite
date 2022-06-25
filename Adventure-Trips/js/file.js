c458750546337982236a879af0a21b7465cca344
Ahmed shalaby <97378406+shalaby2022@users.noreply.github.com>
2022-06-26 00:02:55 +0200

    Add files via upload

---
+++ Adventure-Trips/js/file.js
@@ -0,0 +1,10 @@
+/* p {
+    color: transparent;
+    background: url(../images/animation.webp);
+    width: 50%;
+    box-sizing: border-box;
+    border: 5px dotted red;
+    padding: 30px;
+    background-clip: text;
+    font-family: 'Festive', cursive;
+} */
